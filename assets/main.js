045f1079016d163df88f6c3f194709700cea6987
thinker3197 <ashishgupta.3197@gmail.com>
2016-11-23 02:12:09 +0530

    Make devsoc-matrix great again

--- assets/main.js
+++ assets/main.js
@@ -6,18 +6,21 @@ window.onload = function() {
     xhr.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             var peeps = document.querySelector('#peeps'),
-                data = JSON.parse(this.responseText);
-            console.log(data);
-            shuffle(data);
+                data = JSON.parse(this.responseText),
+                keys = Object.keys(data);
 
-            for(var id in data) {
-                var name = data[id][0],
-                    url = data[id][1];
+            keys.sort(function(a, b){
+                return Math.random() - 0.5;
+            });
+
+            for(var id in keys) {
+                var name = data[keys[id]][0],
+                    url = data[keys[id]][1];
 
                 var a = document.createElement('A'),
                     img = document.createElement('IMG');
                 
-                a.setAttribute('href', 'https://facebook.com/' + id);
+                a.setAttribute('href', 'https://facebook.com/' + keys[id]);
                 img.setAttribute('src', url);
 
                 a.appendChild(img);
@@ -25,7 +28,7 @@ window.onload = function() {
             }
         }
     };
-    
+
     xhr.open("GET", "data.json", true);
     xhr.send();
 };
