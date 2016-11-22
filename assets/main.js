d46888750070aa5ed085f488aff913e23be077a2
thinker3197 <ashishgupta.3197@gmail.com>
2016-11-22 22:54:53 +0530

    Remove unwanted ~300Kb dependencies

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
-$(function() {
-    'use strict';
+'use strict';
+
+window.onload = function() {
 
     // https://css-tricks.com/snippets/javascript/shuffle-array/
     function shuffle(o) {
@@ -7,16 +8,31 @@ $(function() {
         return o;
     };
 
-    $.getJSON('data.json', function(data) {
-        var peeps = $('#peeps');
-        shuffle(data);
-        for (var id in data) {
-            var name = data[id][0];
-            var url = data[id][1];
-            var a = $('<a/>')
-                .prop('href', "http://facebook.com/" + id)
-                .append($('<img>').prop('src', url))
-                .appendTo(peeps);
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var peeps = document.querySelector('#peeps'),
+                data = JSON.parse(this.responseText);
+            console.log(data);
+            shuffle(data);
+
+            for(var id in data) {
+                var name = data[id][0],
+                    url = data[id][1];
+
+                var a = document.createElement('A'),
+                    img = document.createElement('IMG');
+                
+                a.setAttribute('href', 'https://facebook.com/' + id);
+                img.setAttribute('src', url);
+
+                a.appendChild(img);
+                peeps.appendChild(a);
+            }
         }
-    });
-});
+    };
+
+    xhr.open("GET", "data.json", true);
+    xhr.send();
+};
